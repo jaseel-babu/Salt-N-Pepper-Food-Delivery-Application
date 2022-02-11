323201f42652302e0255518b02d94f57118325d0
jaseel-babu <jaseelmancheri@gmail.com>
2022-02-11 11:30:43 +0530

    List Sellers Data

--- lib/view/homepage.dart
+++ lib/view/homepage.dart
@@ -1,8 +1,10 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
+import 'package:user_side/consts/sliderimages.dart';
 import 'package:user_side/controller/apiservices.dart';
 import 'package:user_side/controller/locationControll.dart';
 import 'package:user_side/model/sellermodel.dart';
@@ -14,16 +16,83 @@ class HomePage extends StatelessWidget {
   final locationController = Get.put(LocationController());
   @override
   Widget build(BuildContext context) {
+    final maxWidth = MediaQuery.of(context).size.width;
+    final maxHeight = MediaQuery.of(context).size.width;
     return Scaffold(
       body: SafeArea(
         child: ListView(
-          shrinkWrap: true,
+          // shrinkWrap: true,
           children: [
             ListTile(
+              leading: Icon(
+                Icons.format_align_left,
+                color: Colors.blue[900],
+                size: 30,
+              ),
               trailing: GetBuilder<LocationController>(
                 builder: (locationController) {
                   return Text(locationController.currentAddress!);
                 },
+              ),
+            ),
+            CustomScrollView(
+              shrinkWrap: true,
+              slivers: [
+                SliverToBoxAdapter(
+                  child: Padding(
+                    padding: EdgeInsets.all(1.0),
+                    child: Container(
+                      height: maxHeight * .5,
+                      width: maxWidth * .3,
+                      child: CarouselSlider(
+                        items: sliderItems.map((index) {
+                          return Builder(
+                            builder: (context) {
+                              return Container(
+                                width: maxWidth,
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                    width: 0.5,
+                                  ),
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(2.0),
+                                  child: Image.asset(
+                                    index,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              );
+                            },
+                          );
+                        }).toList(),
+                        options: CarouselOptions(
+                          height: 500,
+                          autoPlay: true,
+                          autoPlayInterval: const Duration(seconds: 3),
+                          autoPlayAnimationDuration:
+                              const Duration(milliseconds: 500),
+                          autoPlayCurve: Curves.decelerate,
+                          enlargeCenterPage: true,
+                          scrollDirection: Axis.horizontal,
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Container(
+              margin: const EdgeInsets.all(30),
+              child: const TextField(
+                decoration: InputDecoration(
+                  label: Text("Search Your Favourite Resturent"),
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                ),
+                textInputAction: TextInputAction.next,
               ),
             ),
             StreamBuilder(
@@ -32,15 +101,41 @@ class HomePage extends StatelessWidget {
                 List<SellerModel> data = [];
                 if (snapshot.hasData) {
                   data = snapshot.data as List<SellerModel>;
-                  print(data[0].sellerName);
-                  return ListView.builder(shrinkWrap: true,
+
+                  return ListView.builder(
+                    physics: const ScrollPhysics(),
+                    shrinkWrap: true,
                     itemCount: data.length,
                     itemBuilder: (context, index) {
-                      return Text(data[index].sellerName!);
+                      return Card(
+                        child: Stack(
+                          alignment: AlignmentDirectional.bottomCenter,
+                          children: [
+                            Image.asset(
+                              sliderItems[sliderItems.length - 1 - index],
+                            ),
+                            Container(
+                              height: maxHeight * 0.1,
+                              width: maxWidth,
+                              color: Colors.black.withOpacity(0.6),
+                              child: Center(
+                                child: Text(
+                                  data[index].shopName!,
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 22,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      );
                     },
                   );
                 }
-                return const Text('false');
+                return const CircularProgressIndicator();
               },
             )
           ],
